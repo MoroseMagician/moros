95339a7439aadb1e52dbfdac5b4ca1bb09ae0cc3
magician <magician@hakke.ro>
2018-12-15 21:51:01 +0000

    Material tab navigation
    This was so easy, what the hell...
    Is this really crossplatform?

--- App.js
+++ App.js
@@ -3,26 +3,75 @@ import {
   StyleSheet, 
   Text, 
   View, 
+  StatusBar,
 } from 'react-native';
+import {
+  createMaterialTopTabNavigator,
+  createAppContainer,
+} from 'react-navigation';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello, world!</Text>
+      <View style={styles.app}>
+        <StatusBar hidden />
+        <AppContainer />
       </View>
     );
   }
 }
 
+// TODO: move these into discrete files
+// TODO: figure out how to handle styles
+//       Inline or use something like LESS?
+class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello, home!</Text>
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello, details!</Text>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createMaterialTopTabNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    tabBarOptions: {
+      animationEnabled: true,
+      swipeEnabled: true
+    }
+  }
+);
+
+const AppContainer = createAppContainer(AppNavigator);
+
 const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     paddingTop: 40,
